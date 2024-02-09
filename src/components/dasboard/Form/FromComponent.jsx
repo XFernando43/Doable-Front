c1b1719f07f29dd90a25dbfc9726724e6c235167
XFernando43 <U20171E997@upc.edu.pe>
2024-02-09 12:39:47 -0500

    add login context to login page

--- src/components/dasboard/Form/FromComponent.jsx
+++ src/components/dasboard/Form/FromComponent.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import ButtonComponent from '../../commons/Button/ButtonComponent'
+import { LoginContext } from '../../../contexts/ContextLogin';
+
+// import { redirect } from "react-router-dom";
+
 import './Form.style.css'
 
 export default function FormComponent(){
     const [Username,setUsername] = React.useState("");
     const [Password,setPassword] = React.useState("");
+    const { userInfo,handleLogin }  = React.useContext(LoginContext);
     
-    const url_To_Login = 'http://localhost:5500/users/SignIn';
-    // const userInfo = React.useContext(LoginContext);
-    // const {token,setToken} = React.useState(localStorage.getItem('token'));
+    const url_To_Login = 'https://bordeable-api.onrender.com/users/SignIn';
 
 
     async function Login(){        
         const data = {
-            username: "XFERNANDO",
-            password: "Mierdamierda123"
+            username: Username,
+            password: Password
         };
 
         try{
@@ -24,89 +27,60 @@ export default function FormComponent(){
             body: JSON.stringify(data)
           }).then(response => response.json())
             .then(result => {
-              console.log("Éxito:", result);
+                console.log("Éxito:", result);
             })
         }catch (error) {
-          window.alert('NO SE PUDO AGREGAR POKEMON A FAVORITOS');
           console.error('Error:', error);
         }
     }
 
     const handleUsernameChange = (event) => {
-        console.log(Username);
         setUsername(event.target.value);
     };
     
     const handlePasswordChange = (event) => {
-        console.log(Password);
         setPassword(event.target.value);
     };
 
     const handleClick = (event) => {
         event.preventDefault(); 
         Login();
-        // window.alert("AC")
-        // console.log(userInfo);
     };  
-
-
+    
+    
+    function prueba(event){
+        event.preventDefault(); 
+              
+        console.log("PRUEBA DE CONTEXT");
+        console.log(userInfo);
+        handleLogin("user2","loaosdkasdj");
+        console.log("PRUEBA DE CONTEXT2");
+        console.log(userInfo);
+    }
 
     return(
-    <>
-        <form className='form'>
-            <div className='input-container'>
-                <label className='label' htmlFor="username">Username</label>
-                <input className='input-form' type="text" id="username" onChange={handleUsernameChange}/>
-            </div>
+        <>
+            <form className='form'>
+                <div className='input-container'>
+                    <label className='label' htmlFor="username">Username</label>
+                    <input className='input-form' type="text" id="username" onChange={handleUsernameChange}/>
+                </div>
 
-            <div className='input-container'>
-                <label className='label' htmlFor="password">password</label>
-                <input className='input-form' type="text" id="password" onChange={handlePasswordChange} />
-            </div>
+                <div className='input-container'>
+                    <label className='label' htmlFor="password">password</label>
+                    <input className='input-form' type="text" id="password" onChange={handlePasswordChange} />
+                </div>
 
-            <ButtonComponent text="Login" type="Primary" size="lg" _function = {handleClick}></ButtonComponent>
+                <ButtonComponent text="Login" type="Primary" size="lg" _function = {handleClick}></ButtonComponent>
+                <ButtonComponent text="Prueba" type="Primary" size="lg" _function = {prueba}></ButtonComponent>
 
-            <a className='link_' href="">Create an Account
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path d="M3.33331 7.99998H12.6666M12.6666 7.99998L7.99998 3.33331M12.6666 7.99998L7.99998 12.6666" stroke="#6D28D9"/>
-                </svg>
-            </a>
+                <a className='link_' href="">Create an Account
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                    <path d="M3.33331 7.99998H12.6666M12.6666 7.99998L7.99998 3.33331M12.6666 7.99998L7.99998 12.6666" stroke="#6D28D9"/>
+                    </svg>
+                </a>
 
-        </form>
-    </>
+            </form>
+        </>
     )
 }
-
-
-// const url_To_Login = 'http://localhost:5500/users/SignIn';
-// const userInfo = React.useContext(LoginContext);
-// const {token,setToken} = React.useState(localStorage.getItem('token'));
-
-
-// async function Login(){
-//     const data = {
-//     username: 'XFERNANDO',
-//     password: 'Mierdamierda123'
-//     };
-
-//     fetch(url_To_Login, {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(data)
-//     })
-//     .then(response => {
-//     if (!response.ok) {
-//         throw new Error('Error en la solicitud');
-//     }
-//     return response.json();
-//     })
-//     .then(data => {
-//     console.log('Respuesta del servidor:', data);
-//     })
-//     .catch(error => {
-//     console.error('Error:', error);
-//     });
-// }
-
--- src/contexts/ContextLogin.jsx
+++ src/contexts/ContextLogin.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { createContext } from "react";
 
-export const LoginContext = createContext({ isLogin: false, username: "", token: "" });
+export const LoginContext = createContext();
 
 const ContextLogin = ({ children }) => {
-  const [userInfo, setUserInfo] = useState({ isLogin: false, username: "", token: "" });
+  const [userInfo, setUserInfo] = useState({ isLogin: false, username: "NONE", token: "TOKEN_NONE" });
   
   function handleLogin(username, token) {
     const userNewInfo = { isLogin: true, username: username, token: token };
