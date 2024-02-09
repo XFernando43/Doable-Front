4afec2e870cab65c868e54728eea9aaee37026b8
XFernando43 <U20171E997@upc.edu.pe>
2024-02-08 21:48:40 -0500

    add card feature

--- src/components/dasboard/Card/CardComponent.jsx
+++ src/components/dasboard/Card/CardComponent.jsx
@@ -3,27 +3,25 @@ import LabelComponent from '../../commons/Label/LabelComponent';
 import ButtonComponent from '../../commons/Button/ButtonComponent';
 import TitleComponent from '../../commons/Title/TitleComponent';
 
-export default function CardComponent({ value ,title}){
-    return(
-        <div className='card_container'>
+export default function CardComponent({ value, title }) {
+    return (
+        <>
             {value === '1' ? (
-                <>      
+                <div className='card_container board_to_create'>
                     <div className='input_container'>
                         <LabelComponent text="Board Title" size="sm" htmlFor="text_input" />
                         <textarea className='text_area' id='text_input' />
                     </div>
-
                     <div className='footer_card'>
-                    <p>color</p>
-                    <ButtonComponent text="Create" type="Primary" />
+                        <p>color</p>
+                        <ButtonComponent text="Create" type="Primary" />
                     </div>
-                </>
-            )
-             :
-                <TitleComponent text={title} size="smm"/>
-             
-            }              
-                
-        </div>
-    )
+                </div>
+            ) : (
+                <div className='card_container align-center board_created'>
+                   <TitleComponent text={title} size="smm"/>
+                </div>
+            )}
+        </>
+    );
 }
